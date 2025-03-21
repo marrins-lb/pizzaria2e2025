642fca7affdf2b3204315782276625b7e1e7abf4
marrins-lb <laylamartins1606@gmail.com>
2025-03-21 09:40:11 -0300

    projeto pizzaria

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+function App() {
+  const [nome, setNome] = useState('Ronaldo')
+  return (
+    <div>
+      <h3>pizzaria2E</h3>
+
+      <input
+        className="nome"
+        onChange={(e)=>{setNome(e.target.value)}}
+        type=" text" />
+      <button
+        className="botao"
+        onClick={() => { alert(nome) }}
+      >
+        CLIQUE AQUI
+      </button>
+
+
+
+    </div>
+  )
+}
+export default App
